e6e965f077494839f8df9fbcd650414b3c054169
obeutz3449 <obeutz3449@sdsu.edu>
2026-09-21 18:21:44 -0700

    Added some Render infastructure

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 //basically backedn api
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+    res.json({
+        users: ["userOne", "userTwo", "userThree"]
+    });
 })
 
 //fetch and display user array
-app.listen(5000, () => { console.log("server started on port 5000") })
-//client port will be at 3000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("server started on port 5000")
+});
